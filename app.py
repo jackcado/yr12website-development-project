11bb9d0f08e1e40eae96439097aa8be9fce3dc4a
jackcado <emaleearnesen@shgcham.school.nz>
2026-05-29 14:57:34 +1200

    database cont

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
+if __name__ == "__main__":
+    app.run(debug=True)
+
 from flask import Flask, render_template
+import sqlite3   #for the database stuff
 
 app = Flask(__name__)
 
+DATABASE = "app.db"
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM item" 
+    cursor.execute(sql)
+    results = cursor.fetchall() 
+    db.close()    
+    return render_template('index.html',results=results) 
 
 if __name__ == "__main__":
     app.run(debug=True)
