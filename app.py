191a607b431a5f9b6636143875af60bd7c788f3e
jackcado <emaleearnesen@shgcham.school.nz>
2026-06-19 09:34:09 +1200

    holy nav bar

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
-import sqlite3   #for the database stuff
+import sqlite3
 
 app = Flask(__name__)
 
-#the path and filename for the database
 DATABASE = "please.db"
 
-#cool function to automatcally connect and query
 def query_db(sql,args=(),one=False):
     '''connect and query- will retun one item if one=true and can accept arguments as tuple'''
     db = sqlite3.connect(DATABASE)
@@ -15,17 +13,35 @@ def query_db(sql,args=(),one=False):
     results = cursor.fetchall()
     db.commit()
     db.close()
-    #return None if ther is no result from the query
+    #return None if there is no result from the query
     #return the first item only if one=True
     #return the list of tuples if one=False
     return (results[0] if results else None) if one else results
 
 
-#routes go here
+#routes
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/home')
+def home():
+    return render_template('index.html')
 
-#this bit of code runs the app that we just made with debug on
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/report')
+def report():
+    return render_template('report.html')
+
+@app.route('/searchreports')
+def searchreports():
+    return render_template('searchreports.html')
+
+@app.route('/speciesfinder')
+def speciesfinder():
+    return render_template('speciesfinder.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
